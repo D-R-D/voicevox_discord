3b4ec94ecb8238bfa4c187d46704702e563e59b7
DDD <77850088+D-R-D@users.noreply.github.com>
2023-03-15 13:14:07 +0900

    chatgptのInitialMessageとGptModelをSettingsから取得するようにした

--- discord_template/Settings.cs
+++ discord_template/Settings.cs
@@ -1,4 +1,8 @@
-﻿using System.Configuration;
+﻿using Discord;
+using System;
+using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 using System.Xml.Linq;
 
 namespace voicevox_discord
@@ -14,14 +18,18 @@ namespace voicevox_discord
 
 
         public readonly string m_OpenAIKey;
+        public readonly string m_GptModel; // chatgptのバージョン
+        public readonly string m_GptInitialMessage;// chatgptの初期化メッセージ
+
         public readonly string m_Token;
-        public readonly string[] m_GuildIds;
-        public readonly string m_ApplicationId;
-        public readonly string[] m_AdminIds;
         public readonly string m_DiscordAPIVersion;
+        public readonly string m_ApplicationId;
+        public readonly string[] m_GuildIds;
+        public readonly string[] m_AdminIds;
         public readonly IReadOnlyDictionary<string, VoicevoxEngineApi> m_EngineDictionary;
 
 
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
         public Settings()
         {
 
@@ -29,27 +37,35 @@ namespace voicevox_discord
             var reader = new AppSettingsReader();
 
             m_OpenAIKey = (string)reader.GetValue("OPENAI_APIKEY", typeof(string));
-            if (m_OpenAIKey.IsNullOrEmpty()) { throw new Exception("OPENAI_APIKEY。\ntokenがnullもしくは空白です。"); }
+            if (m_OpenAIKey.IsNullOrEmpty()) { throw new Exception($"{nameof(m_OpenAIKey)}.\nAPIKEYがnullもしくは空白です。"); }
+
+            m_GptModel = (string)reader.GetValue("gpt_model", typeof(string));
+            if (m_OpenAIKey.IsNullOrEmpty()) { throw new Exception($"{nameof(m_GptModel)}.\ngpt_modelがnullもしくは空白です。"); }
+
+            m_GptModel = (string)reader.GetValue("gpt_initialmessage", typeof(string));
+            if (m_OpenAIKey.IsNullOrEmpty()) { throw new Exception($"{nameof(m_GptModel)}.\ngpt_initialmessageがnullもしくは空白です。"); }
 
             //config内のtokenを取得する
             m_Token = (string)reader.GetValue("token", typeof(string));
-            if (m_Token.IsNullOrEmpty()) { throw new Exception("tokenが不正です。\ntokenがnullもしくは空白です。"); }
+            if (m_Token.IsNullOrEmpty()) { throw new Exception($"{nameof(m_Token)}\ntokenがnullもしくは空白です。"); }
 
-            //config内の","で区切られたguild_idを取得する
-            var guildId = (string)reader.GetValue("guild_id", typeof(string));
-            if (guildId.IsNullOrEmpty()) { throw new Exception("guild_idが不正です。\nguild_idがnullもしくは空白です。"); }
-            m_GuildIds = guildId!.Split(',');
+            //config内のdiscordapi_versionを取得する
+            m_DiscordAPIVersion = (string)reader.GetValue("discordapi_version", typeof(string));
+            if (m_DiscordAPIVersion.IsNullOrEmpty()) { throw new Exception($"{m_DiscordAPIVersion}.\ndiscordapi_versionがnullもしくは空白です。"); }
 
             //config内のapplication_idを取得する
             m_ApplicationId = (string)reader.GetValue("application_id", typeof(string));
-            if (m_ApplicationId.IsNullOrEmpty()) { throw new Exception("application_idが不正です。\napplication_idがnullもしくは空白です。"); }
+            if (m_ApplicationId.IsNullOrEmpty()) { throw new Exception($"{nameof(m_ApplicationId)}.\napplication_idがnullもしくは空白です。"); }
 
+            //config内の","で区切られたguild_idを取得する
+            var guildId = (string)reader.GetValue("guild_id", typeof(string));
+            if (guildId.IsNullOrEmpty()) { throw new Exception($"{nameof(guildId)}.\nguild_idがnullもしくは空白です。"); }
+            m_GuildIds = guildId!.Split(',');
+
+            //config内の","で区切られたadmin_idを取得する
             string adminId = (string)reader.GetValue("admin_id", typeof(string));
-            if (adminId.IsNullOrEmpty()) { throw new Exception("admin_id。\napplication_idがnullもしくは空白です。"); }
+            if (adminId.IsNullOrEmpty()) { throw new Exception($"{nameof(adminId)}.\nadmin_idがnullもしくは空白です。"); }
             m_AdminIds = adminId!.Split(',');
-
-            m_DiscordAPIVersion = (string)reader.GetValue("discordapi_version", typeof(string));
-            if (m_DiscordAPIVersion.IsNullOrEmpty()) { throw new Exception("discordapi_version。\napplication_idがnullもしくは空白です。"); }
             #endregion
 
             #region XMLファイルからのエンジンリスト読み込み
@@ -73,5 +89,6 @@ namespace voicevox_discord
             #endregion
 
         }
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
     }
 }
